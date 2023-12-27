39d58b7d5865ae3c64da000672f6bf372e8a02b6
takashiono2 <takashiono710@gmail.com>
2023-12-28 00:27:05 +0900

    section8

--- webpack.config.js
+++ webpack.config.js
@@ -58,6 +58,10 @@ module.exports = {
       template: './src/templates/index.pug',
       filename: 'index.html',
     }),
+    new HtmlWebpackPlugin({
+      template: './src/templates/access.pug',
+      filename: 'access.html'
+    }),
     new CleanWebpackPlugin(),
   ],
 }
