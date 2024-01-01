1622bfe53f555545fc6c2830eb77bdcf2a0f4c00
takashiono2 <takashiono710@gmail.com>
2024-01-02 00:55:39 +0900

    section18

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   entry: './src/javascripts/main.js',
   output: {
     path: path.resolve(__dirname,'./dist'),
-    filename: 'javascripts/main.js',
+    filename: 'javascripts/[name]-[contenthash].js',
   },
   module:{
     rules: [
@@ -57,7 +57,7 @@ module.exports = {
         test: /\.(png|jpg|jpeg)$/i,
         type: 'asset/resource',
         generator: {
-          filename: 'images/[name][ext]'
+          filename: 'images/[name]-[contenthash][ext]'
         },
         use: [
           {
@@ -96,7 +96,7 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: './stylesheets/main.css',
+      filename: './stylesheets/[name]-[contenthash].css',
     }),
     new HtmlWebpackPlugin({
       template: './src/templates/index.pug',
